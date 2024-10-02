a10100d989cfaefd7918bb6d81d02c223bed2994
Michael Manning <michael@manning390.com>
2024-10-01 20:19:47 -0700

    Init commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        accent: '#6592a7',
+      }
+    },
+  },
+  plugins: [],
+}
+
